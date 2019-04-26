95f24da881872f807fada20c1b51196f51ac1fb8
Hacksdream <15112624992@163.com>
2019-04-27 00:01:04 +0800

    2019-4-26 leetcode record

---
+++ LinkList/linked_del_node.py
@@ -0,0 +1,51 @@
+# -*- coding:utf-8 -*-
+
+class Node():
+    def __init__(self,data=None):
+        self.data = data
+        self.next = None
+
+class LinkedList():
+    def __init__(self):
+        self.head = None
+
+    def print_list(self):
+        node1 = self.head
+        while node1:
+            print(node1.data)
+            node1 = node1.next
+    
+    def del_node(self,node_key):
+        if self.head == None:
+            return
+
+        head_value = self.head.data
+        if head_value == node_key:
+            self.head = self.head.next
+        
+        node = self.head
+        while node:
+            pre = node
+            if node.data == node_key:
+                node =  node.next
+                return
+            node = node.next
+
+n1 = Node("1")
+n2 = Node("2")
+n3 = Node("3")
+n1.next = n2
+n2.next = n3
+
+list = LinkedList()            
+list.head = n1
+print("删除前：")
+list.print_list()
+
+print("-"*50)
+print("删除后：")
+list.del_node("2")
+
+list.print_list()
+
+            
